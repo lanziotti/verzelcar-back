6aeadd3804bd8e1f073e72266adbe71e1dc7b310
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-11-02 14:41:32 -0300

    Configurando o banco de dados para deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
+        host: process.env.KNEX_HOST,
+        port: process.env.KNEX_PORT,
+        user: process.env.KNEX_USER,
+        password: process.env.KNEX_PASS,
+        database: process.env.KNEX_NAME,
         ssl: { rejectUnauthorized: false }
     }
 });
